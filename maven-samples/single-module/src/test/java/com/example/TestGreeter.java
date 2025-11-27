174e83cee67e05ff30dc3b117d996afc795524dd
Noman <alnoman05405@gmail.com>
2025-11-27 07:43:33 +0600

    Add test for greetShouldFailed method

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,4 +41,9 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldFailed() {
+    assertTrue(false);
+  }
 }
